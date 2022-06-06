aefba4f7fe830e63ca36aa26fcd91232b6af4cfa
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2022-06-06 16:16:39 -0300

    chore: add react-query and axios config

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+
 import { Home } from './pages/Home';
+import { queryClient } from './services/queryClient';
 
 import './styles/global.css';
 
+const isDev = process.env.NODE_ENV === 'development';
+
 export function App() {
-  return <Home />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Home />
+      {isDev && <ReactQueryDevtools />}
+    </QueryClientProvider>
+  );
 }
