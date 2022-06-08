99ccb549dc3906fbfcd2ab04b5d5cb0cfc5889fc
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2022-06-08 17:50:13 -0300

    chore: remove constant CURRENT_PROFILE

--- src/constants/currentProfile.ts
+++
@@ -1,5 +0,0 @@
-export default {
-  avatarUrl: 'https://github.com/luiz21goncalves.png',
-  name: 'Luiz Gonçalves',
-  role: 'Desenvolveder Web',
-};
--- src/constants/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as CURRENT_PROFILE } from './currentProfile';
